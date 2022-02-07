c09891b1dda247814cd11cb6eea8b6fa3dba9ccc
Czencz Family <czenczfamily@Czenczs-MacBook-Pro.local>
2022-02-06 21:25:46 -0700

    Added basic navbar starter

--- template-two/components/navbar.js
+++ template-two/components/navbar.js
@@ -3,16 +3,31 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <>
-            <Link href="/">
-                <a>Home</a>
-            </Link>
-            <Link href="/about">
-                <a>About</a>
-            </Link>
-            <Link href="/contact">
-                <a>Contact</a>
-            </Link>
-        </>
+        <header>
+            <nav class="navbar navbar-dark navbar-expand-lg navbar-light bg-primary">
+                <div class="container-fluid">
+                    <Link href="/">
+                        <a class="navbar-brand">Logo / Name</a>
+                    </Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <Link href="/about">
+                                <a class="nav-link" aria-current="page" href="#">About us</a>
+                            </Link>
+                        </li>
+                        <li class="nav-item">
+                        <Link href="/contact">
+                                <a class="nav-link" aria-current="page" href="#">Contact us</a>
+                            </Link>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
     )
 }
--- template-two/pages/_app.js
+++ template-two/pages/_app.js
@@ -14,6 +14,7 @@ export default function MyApp({ Component, pageProps }) {
     <>
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
         </Head>
         <Navbar/>
         <Component {...pageProps} />
